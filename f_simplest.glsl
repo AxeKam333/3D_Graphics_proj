4712972b5b3cbd903dea87acbdd44336449ca456
AxeKam333 <olek.kaminski5@gmail.com>
2024-05-21 18:32:14 +0200

    Drugie źródło światła

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -9,24 +9,36 @@ in vec2 iTexCoord0;
 in vec2 iTexCoord1;
 in vec4 ic; 
 in vec4 n;
-in vec4 l;
+in vec4 l1;
+in vec4 l2;
 in vec4 v;
 
 void main(void) {
 
 	//Znormalizowane interpolowane wektory
-	vec4 ml = normalize(l);
+	vec4 ml1 = normalize(l1);
+	vec4 ml2 = normalize(l2);
 	vec4 mn = normalize(n);
 	vec4 mv = normalize(v);
 	//Wektor odbity
-	vec4 mr = reflect(-ml, mn);
+	vec4 mr1 = reflect(-ml1, mn);
+	vec4 mr2 = reflect(-ml2, mn);
 
 	//Parametry powierzchni
 	vec4 kd = mix(texture(textureMap0, iTexCoord0), texture(textureMap1, iTexCoord1),0.3);
 	vec4 ks = vec4(1, 1, 1, 1);
 
 	//Obliczenie modelu oświetlenia
-	float nl = clamp(dot(mn, ml), 0, 1);
-	float rv = pow(clamp(dot(mr, mv), 0, 1), 50);
-	pixelColor= vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+    float nl1 = clamp(dot(mn, ml1), 0, 1);
+    float rv1 = pow(clamp(dot(mr1, mv), 0, 1), 50);
+
+    float nl2 = clamp(dot(mn, ml2), 0, 1);
+    float rv2 = pow(clamp(dot(mr2, mv), 0, 1), 50);
+
+    //Kumulacja oświetlenia dla obu źródeł
+    vec4 lightColor1 = vec4(kd.rgb * nl1, kd.a) + vec4(ks.rgb * rv1, 0);
+    vec4 lightColor2 = vec4(kd.rgb * nl2, kd.a) + vec4(ks.rgb * rv2, 0);
+
+    //Finalny kolor piksela
+    pixelColor = lightColor1 + lightColor2;
 }
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -13,15 +13,20 @@ in vec2 texCoord0;
 
 //Zmienne interpolowane
 out vec4 ic;
-out vec4 l;
+out vec4 l1;
+out vec4 l2;
 out vec4 n;
 out vec4 v;
 out vec2 iTexCoord0; 
 out vec2 iTexCoord1;
 
 void main(void) {
-    vec4 lp = vec4(0, 0, 6, 1); //pozcyja światła, przestrzeń świata
-    l = normalize(V * lp - V*M*vertex); //wektor do światła w przestrzeni oka
+    vec4 lp1 = vec4(5, 0, 6, 1); //pozcyja 1 światła, przestrzeń świata
+    vec4 lp2 = vec4(-100, 0, 6, 1); //pozcyja 2 światła, przestrzeń świata
+
+    l1 = normalize(V * lp1 - V*M*vertex); //wektor do światła w przestrzeni oka
+    l2 = normalize(V * lp2 - V*M*vertex); //wektor do światła w przestrzeni oka
+
     v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
     n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
     
